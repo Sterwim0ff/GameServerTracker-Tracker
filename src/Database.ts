c824243753a60f786ef177b441fd9acaad2eb66a
Sterwim0ff <sterwim.officiel@gmail.com>
2023-03-05 12:57:13 +0100

    0.0.4

--- src/Database.ts
+++ src/Database.ts
@@ -3,12 +3,12 @@ import { ServerData } from "./ServerData";
 import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.MYSQL_HOST || "localhost",
+    type: process.env.DB_TYPE ||"mysql",
+    host: process.env.DB_HOST || "localhost",
     port: 3306,
-    username: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "root",
-    database: process.env.MYSQL_DB || "server",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_DB || "server",
     synchronize: true,
     logging: false,
     entities: [ServerData],
